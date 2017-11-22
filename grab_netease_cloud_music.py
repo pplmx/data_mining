e2ca526ed3803c2305a95109ef7ac2a6b0361453
mystic-cg <215104920@qq.com>
2017-11-22 21:02:38 +0800

    grab netease cloud music

---
+++ grab_netease_cloud_music.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by PyCharm
+# @author  : mystic
+# @date    : 2017/11/22 20:14
+"""
+    抓取网易云音乐歌词
+"""
+import re
+import urllib.request
+import urllib
+
+url = "http://www.qiushibaike.com"
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+req = urllib.request.Request(url, headers={
+    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+})
+response = urllib.request.urlopen(req)
+content = response.read().decode('utf-8')
+# print(content)
+pattern = re.compile('<div.*?class="content">\n*?<span.*?</span>\n*?</div>')
+items = re.findall(pattern, content)
+for item in items:
+    print(item)

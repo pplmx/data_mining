121eefd48432827fbd46c5dfd21bd119ef1a39f8
mystic_cg <215104920@qq.com>
2017-12-13 17:33:30 +0800

    data analysis

--- grab_qq_zone.py
+++ grab_qq_zone.py
@@ -15,9 +15,12 @@ from collections import Counter
 import jieba
 import xlrd as xlrd
 import xlwt as xlwt
+import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 from numpy import array
+from scipy.misc import imread
 from selenium import webdriver
+from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
 from xlutils.copy import copy
 
 
@@ -131,8 +134,30 @@ def shuoshuo_analysis(file_path):
         # 去除长度为1的词
         # words = list(filter(lambda word: len(word) > 1, words))
         print(Counter(words))
-        # seg_list = jieba.cut(contents[0], HMM=True)
-        # print("/".join(seg_list))
+        back_color = imread('background.jpg')  # 解析该图片
+        wc = WordCloud(background_color='white',  # 背景颜色
+                       max_words=1000,  # 最大词数
+                       mask=back_color,  # 以该参数值作图绘制词云，这个参数不为空时，width和height会被忽略
+                       max_font_size=100,  # 显示字体的最大值
+                       stopwords=STOPWORDS.add('苟利国'),  # 使用内置的屏蔽词，再添加'苟利国'
+                       font_path="C:/Windows/Fonts/STFANGSO.ttf",  # 解决显示口字型乱码问题，可进入C:/Windows/Fonts/目录更换字体
+                       random_state=42,  # 为每个词返回一个PIL颜色
+                       # width=1000,  # 图片的宽
+                       # height=860  #图片的长
+                       )
+        wc.generate(' '.join(words))
+        # 基于彩色图像生成相应彩色
+        image_colors = ImageColorGenerator(back_color)
+        # 显示图片
+        plt.imshow(wc)
+        # 关闭坐标轴
+        plt.axis('off')
+        # 绘制词云
+        plt.figure()
+        plt.imshow(wc.recolor(color_func=image_colors))
+        plt.axis('off')
+        # 保存图片
+        wc.to_file('wordcloud3.png')
 
 
 if __name__ == '__main__':
